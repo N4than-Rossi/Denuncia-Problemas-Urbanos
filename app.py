fd2069eb0433de0933f0d7d29717b447dfc6ac8d
Kev1n269 <knrbadaro@gmail.com>
2025-11-06 16:26:38 -0300

    Criar página de enviar denuncia

--- app.py
+++ app.py
@@ -1,7 +1,23 @@
-from flask import Flask, render_template, redirect, url_for, request;
+from flask import Flask, render_template, redirect, url_for, request, jsonify
+import requests
 import csv
+from mimetypes import guess_type
+from werkzeug.utils import secure_filename
+from datetime import datetime 
+import os
 
 app = Flask(__name__)
+
+UPLOAD_FOLDER='static/assets/problem_images'
+app.config['UPLOAD_FOLDER']=UPLOAD_FOLDER
+os.makedirs(UPLOAD_FOLDER,exist_ok=True)
+
+def allowed_file(filename):
+    mime_type,_=guess_type(filename)
+    if mime_type is None:
+        return False
+    return mime_type.startswith(('image/','video/'))
+
 
 @app.get("/")
 def index():
@@ -11,6 +27,82 @@ def index():
         for row in csv_reader:
             problemas.append(row)
     return render_template("index.html", problemas=problemas)
+
+@app.get("/send")
+def show_report_page():
+    return render_template('send-report.html')
+
+@app.post("/send")
+def send_report():
+    id=1
+    with open('problemas.csv',mode='r',encoding='utf-8') as file:
+        reader=list(csv.reader(file))
+        if len(reader)>1:
+            last_row=reader[-1]
+            if last_row:
+                id+=int(last_row[0])
+
+    if 'foto' not in request.files:
+        return redirect(url_for('show_report_page'))
+    file=request.files["foto"]
+    if file.filename=='' or not allowed_file(file.filename):
+        return redirect(url_for("show_report_page"))
+    filename=secure_filename(file.filename)
+    timestamp=datetime.now().strftime("%Y%m%d_%H%M%S_")
+    filename=timestamp+filename
+    file.save(os.path.join(app.config['UPLOAD_FOLDER'],filename))
+    img_url=url_for('static',filename='assets/problem_images/'+filename)
+
+    name=request.form.get("report-type")
+    adress=request.form.get("address")
+    lat=request.form.get('latitude')
+    lon=request.form.get('longitude')
+    type=request.form.get("type")
+    description=request.form.get("problem")
+    cpf=request.form.get("cpf")
+    new_report=[
+        id,
+        name,
+        img_url,
+        "Em andamento",
+        type,
+        adress,
+        lat,
+        lon,
+        description,
+        cpf
+    ]
+    with open("problemas.csv", mode='a',encoding='utf-8',newline="") as problems:
+        writer=csv.writer(problems)
+        writer.writerow(new_report)
+    return redirect(url_for('index'))
+
+@app.route('/api/nomination',methods=['GET'])
+def nomination():
+    address=request.args.get('address')
+
+    if not address:
+        return jsonify({'error:': "Endereço não fornecido"}), 400
+    try:
+        url="https://nominatim.openstreetmap.org/search"
+        response=requests.get(
+            url,
+            params={
+                'q':address,
+                'format':'json',
+                'limit': 1,
+                'countrycodes':'br'
+            },
+            headers={
+            'User-Agent': "UrbanWeb/1.0 (https://github.com/N4than-Rossi/Denuncia-Problemas-Urbanos/tree/main)",
+            'accept-language': "pt-BR"             
+            },
+            timeout=(5,10)
+        )
+        response.raise_for_status()
+        return jsonify(response.json())
+    except requests.RequestException as error:
+        return jsonify({'error': str(error)}),500
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ static/js/form.js
@@ -0,0 +1,277 @@
+let button_selected;
+let type_selected;
+
+function typeButtonAction(button,text,other_text, type){
+    //função que ocorre quando um dos botões de tipo é clicado
+    if(button_selected)
+button_selected.style.color=''; //estilo do botão normal
+button_selected=button;
+button_selected.style.color='red'; //estilo do botão selecionado
+if(button_selected.textContent==='Outros') text.style.display='block';
+else text.style.display='none';
+other_text.style.display='none';
+type_selected=type;
+}
+
+function validateCpf(cpf){
+    let isPossible=false;
+    let previous_number="X";
+    for (let i of cpf){
+        if(i>='0' && i<='9'){
+            if(previous_number==='X'){
+                previous_number=i;
+                continue;
+            }
+            if(previous_number!==i){
+                isPossible=true;
+                break;
+            }
+        }
+    }
+    if(!isPossible) return false;
+    let check1=parseInt(cpf[0])*10+parseInt(cpf[1])*9+parseInt(cpf[2])*8+parseInt(cpf[4])*7+parseInt(cpf[5])*6+parseInt(cpf[6])*5+parseInt(cpf[8])*4+parseInt(cpf[9])*3+parseInt(cpf[10])*2;
+    check1%=11;
+    if(check1<2) check1=0;
+    else check1=11-check1;
+      let check2=parseInt(cpf[0])*11+parseInt(cpf[1])*10+parseInt(cpf[2])*9+parseInt(cpf[4])*8+parseInt(cpf[5])*7+parseInt(cpf[6])*6+parseInt(cpf[8])*5+parseInt(cpf[9])*4+parseInt(cpf[10])*3+check1*2;
+    check2%=11;
+    if(check2<2) check2=0;
+    else check2=11-check2;
+if(parseInt(cpf[12])===check1 && parseInt(cpf[13])===check2) return true;
+else return false;
+}
+
+const form=document.getElementById('report-form');
+const location_button=document.getElementById('actual-location');
+const address=document.getElementById('address');
+const latInput=document.getElementById('latitude');
+const lonInput=document.getElementById('longitude');
+const addressAlert=document.getElementById('address-alert');
+let using_actual_location=false;
+let currentAddress="";
+async function convertCoordinates(){
+    //Função que converte o adress em latitude e longitude
+    console.log("iniciando conversão...");   
+    if(address.value.trim() && (!currentAddress || currentAddress!==address.value)){
+    try{
+  const response= await fetch(
+    `/api/nomination?address=${encodeURIComponent(address.value)}`
+  );
+  const data= await response.json();
+  if(!data || data.length===0){
+    addressAlert.style.display='block';
+    return;
+  }
+
+if(!data[0].lat || !data[0].lon){
+    addressAlert.style.display='block';
+    return
+}
+
+  latInput.value=data[0].lat;
+  lonInput.value=data[0].lon;
+  addressAlert.style.display='none';
+  currentAddress=address.value;
+  console.log("coordenadas encontradas com sucesso!");
+  console.log("lat:",latInput.value,"lon:",lonInput.value);
+    } catch(error){
+     addressAlert.style.display='block';
+     console.log("Erro em encontrar as coordenadas:");
+     console.log("Erro:",error);
+    }
+}else console.log(Boolean(address.value.trim()),Boolean(!currentAddress || currentAddress!==address.value));
+}
+
+location_button.addEventListener('click', async ()=>{
+    //lógica de encontrar localização atual
+    if(!navigator.geolocation){
+        alert("Seu navegador não suporta geolocalização");
+        return;
+    }
+
+    location_button.textContent="Obtendo localização...";
+    location_button.disabled=true;
+
+    const nomination_url="https://nominatim.openstreetmap.org/reverse?";
+
+    const options={
+        enableHighAccuracy: true,   
+        timeout: 20000,
+        maximumAge: 0
+    };
+
+    navigator.geolocation.getCurrentPosition(
+     async (position) => {
+     const lat=position.coords.latitude;
+     const lon=position.coords.longitude;
+     try{
+       const response= await fetch(
+            nomination_url+`lat=${lat}&lon=${lon}&format=json`,
+        );
+        const data= await response.json();
+       const addressParts=data.address;
+       let parts=[];
+        if(addressParts.road){
+            if(addressParts.house_number) parts.push(addressParts.road+', '+addressParts.house_number);
+            else parts.push(addressParts.road);
+        }
+        if(addressParts.neighbourhood ) parts.push(addressParts.neighbourhood);
+        else if(addressParts.suburb) parts.push(addressParts.suburb);
+        else if (addressParts.quarter) parts.push(addressParts.quarter);
+        if(addressParts.city) parts.push(addressParts.city);
+        else if(addressParts.town) parts.push(addressParts.town);
+        else if(addressParts.village) parts.push(addressParts.village);
+        else if(addressParts.municipality) parts.push(addressParts.municipality);
+        if(addressParts.state) parts.push(addressParts.state);
+        if(addressParts.postcode) parts.push(addressParts.postcode);
+
+        let fullAddress=parts.join(', ');
+
+        if(!fullAddress || fullAddress.trim()==='') fullAddress=data.display_name;
+     address.value=fullAddress;
+    currentAddress=fullAddress;
+     latInput.value=lat;
+     lonInput.value=lon;
+     using_actual_location=true;
+    addressAlert.style.display='none';
+     location_button.textContent="Usar localização atual";
+     location_button.disabled=false;
+     console.log("Local encontrado com sucesso!");
+     console.log('lat:',lat,"lon:",lon);
+     console.log("imprecisão de um raio de",position.coords.accuracy,'metros');
+     }
+     catch(error){
+        console.log("erro na geolocalização:", error);
+        console.log('lat:',lat,"lon:",lon);
+        alert("Não foi possível obter o endereço, Digite manualmente ou tente novamente mais tarde");
+        location_button.textContent="Usar localização atual";
+        location_button.disabled=false;
+        latInput.value=lat;
+        lonInput.value=lon;
+     }
+     },
+     async(error)=>{
+        let errorMessage;
+        switch(error.code){
+            case error.PERMISSION_DENIED:
+                errorMessage="Localização atual não permitida";
+                break;
+            case error.POSITION_UNAVAILABLE:
+                errorMessage="Localização indisponível no momento";
+                break;  
+            case error.TIMEOUT:
+                errorMessage="Tempo limite esgotado. Tente novamente";
+                break;
+            default:
+                errorMessage="Erro desconhecido ao obter localização. Tente novamente";
+        }
+        alert(errorMessage);
+        location_button.textContent = 'Usar localização atual';
+        location_button.disabled = false;
+     },
+     options
+    );
+});
+
+address.addEventListener('focus',()=>addressAlert.style.display='none');
+
+let debounceTimer;
+address.addEventListener('blur',()=>{
+    clearTimeout(debounceTimer);
+    debounceTimer=setTimeout(convertCoordinates,500);
+});
+
+//Verificação de tipo do problema
+const urban_buttons=document.querySelectorAll('.urban-type-button');
+const school_buttons=document.querySelectorAll('.school-type-button');
+const urban_specify=document.getElementById('urban-specify');
+const school_specify=document.getElementById('school-specify');
+for(let button of urban_buttons)
+    button.addEventListener('click', ()=>typeButtonAction(button, urban_specify, school_specify, "Urbano"));
+for(let button of school_buttons)
+    button.addEventListener('click', ()=>typeButtonAction(button, school_specify, urban_specify, "Escolar"));
+
+const reportType=document.getElementById('report-type');
+
+
+const cpf=document.getElementById('cpf');
+const cpfAlert=document.getElementById('cpf-alert');
+cpf.addEventListener('input',()=>{
+    //completa cpf na escrita e verifica se está correto matemáticamente
+let value=cpf.value.replace(/\D/g, '');
+if(value.length>11) value=value.slice(0,11);
+if(value.length>9) value=value.replace(/^(\d{3})(\d{3})(\d{3})(\d{0,2})$/,"$1.$2.$3-$4");
+else if(value.length>6) value=value.replace(/^(\d{3})(\d{3})(\d{0,3})$/,"$1.$2.$3");
+else if(value.length>3) value=value.replace(/^(\d{3})(\d{0,3})$/,"$1.$2");
+
+if(value.length==14){
+    let isValid=validateCpf(value);
+    if(!isValid) cpfAlert.style.display='block';
+    else cpfAlert.style.display='none';
+}else cpfAlert.style.display='none';
+cpf.value=value;
+}
+);
+
+cpf.addEventListener('blur',()=>{
+let value=cpf.value;
+if(value.length && (value.length!==14 || !validateCpf(value)))
+    cpfAlert.style.display='block';
+});
+
+const cpfButton=document.querySelector('.cpf-button');
+let isAnonymous=false;
+cpfButton.addEventListener('click',()=>{
+    if(isAnonymous){
+        //estilo normal (botão e campo de inserir cpf)
+        cpfButton.style.backgroundColor='white';
+        cpf.style.backgroundColor='white';
+        isAnonymous=false;
+        cpf.disabled=false;
+        if(cpf.value.length>0 && (cpf.value.length!==14 || !validateCpf(cpf.value)))
+            cpfAlert.style.display='block';
+    }
+    else{
+        //estilo quando está selecionado (botão e campo de inserir cpf)
+     cpfButton.style.backgroundColor='green';
+     cpf.style.backgroundColor='#f7f7f7';
+     isAnonymous=true;
+     cpf.disabled=true;
+     cpfAlert.style.display='none';
+    }
+});
+
+//submissão do form
+const type=document.getElementById("type");
+form.addEventListener('submit', (event)=>{
+    if(!address.value && !using_actual_location){
+        event.preventDefault();
+        addressAlert.style.display='block';
+        return;
+    }
+   convertCoordinates();
+    if(!button_selected)
+        reportType.value='Outros';
+    else if(button_selected.textContent==="Outros"){
+        if(button_selected===urban_buttons[urban_buttons.length-1] && urban_specify.value.length)
+            reportType.value=urban_specify.value;
+        else if(button_selected===school_buttons[school_buttons.length-1] && school_specify.value.length)
+            reportType.value=school_specify.value;
+        else
+            reportType.value="Outros";
+    }
+    else 
+        reportType.value=button_selected.textContent;
+    if(!type_selected)
+        type.value="Urbano";
+    else 
+        type.value=type_selected;
+        
+    if(isAnonymous || !cpf.value.length) 
+        cpf.value="anonymous";  
+    else if(cpf.value.length<14 || !validateCpf(cpf.value)){
+        event.preventDefault();
+        cpfAlert.style.display='block';
+    }
+}
+);    
